724f1f470c5282fa478a2ff46ca5c235b3f182b2
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2026-03-05 13:06:37 +0600

    css move into index.css

--- src/Section/Project.jsx
+++ src/Section/Project.jsx
@@ -5,10 +5,10 @@ const project = () => {
     <div>
       <div className="container px-3.75">
         <div className="flex gap-7.5">
-          <div className='w-[296.25px] border border-tomato/16 rounded-[20px] text-center pt-[43.5px]'>
-            <img className='w-12.5 h-12.5 mx-auto' src="./src/assets/images/web-design.png" alt="" />
-            <h2 className='font-bold leading-8.5 text-2xl mt-7.5 mb-2.25'>Web Design</h2>
-            <p className='font-normal text-base leading-6.5 font-rubik pb-10.25'>120 Projects</p>
+          <div className='project-card'>
+            <img className='project-img' src="./src/assets/images/web-design.png" alt="" />
+            <h2 className='project-card-title'>Web Design</h2>
+            <p className='project-card-para'>120 Projects</p>
         </div>
         </div>
         
