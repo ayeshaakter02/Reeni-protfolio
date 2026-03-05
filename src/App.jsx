29ecc4106acce3f3204d1bbed55eee13f08dc452
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2026-03-05 11:12:07 +0600

    project-design 1st part design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Project from './Section/Project'
 
 const App = () => {
   return (
     <>
     <Navbar/>
+    <Project/>
     </>
   )
 }
