0a9640ee6cb0c8fe2b8e98c2b5794a3d36c1d065
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2026-03-05 16:39:21 +0600

    project part fully complete with rresponsive

--- src/Section/Project.jsx
+++ src/Section/Project.jsx
@@ -3,13 +3,28 @@ import React from 'react'
 const project = () => {
   return (
     <div>
-      <div className="container px-3.75">
-        <div className="flex gap-7.5">
+      <div className="container px-3.75 my-7.5 lg:my-22.5">
+        <div className="sm:grid sm:grid-cols-2 md:flex justify-center place-items-center gap-2.5 lg:gap-7.5 mt-7.5">
           <div className='project-card'>
             <img className='project-img' src="./src/assets/images/web-design.png" alt="" />
             <h2 className='project-card-title'>Web Design</h2>
             <p className='project-card-para'>120 Projects</p>
-        </div>
+          </div>
+          <div className='project-card'>
+            <img className='project-img' src="./src/assets/images/ui-ux-design.png" alt="" />
+            <h2 className='project-card-title'>Ui/Ux Design</h2>
+            <p className='project-card-para'>241 Projects</p>
+          </div>
+          <div className='project-card'>
+            <img className='project-img' src="./src/assets/images/research-design.png" alt="" />
+            <h2 className='project-card-title'>Web Research</h2>
+            <p className='project-card-para'>240 Projects</p>
+          </div>
+          <div className='project-card'>
+            <img className='project-img' src="./src/assets/images/marketing-design.png" alt="" />
+            <h2 className='project-card-title'>Marketing</h2>
+            <p className='project-card-para'>331 Prodect</p>
+          </div>
         </div>
         
       </div>
